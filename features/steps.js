a652fbf9cc2fc6d4653e7e302b24f8ab4b968a17
ProninDmitry <slidernode@yandex.ru>
2020-03-27 11:59:54 +0300

    fix

--- features/steps.js
+++ features/steps.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-restricted-syntax */
 const { Given, Then, When } = require('cucumber');
 const request = require('supertest');
+const assert = require('assert');
 const controller = require('../src/game.js');
 const app = require('../src/server.js');
+
+let tempField = [];
 
 // eslint-disable-next-line no-unused-vars
 let lastResult = {};
@@ -22,5 +26,47 @@ Given('игрок ходит в клетку {int}, {int}', (x, y) => {
 });
 
 Then('поле становится {string}', (str) => {
+  if (controller.getStatus() === 'Ok') {
+    let tempStr = '';
+    for (const s of str) {
+      if (s !== '|') tempStr += s;
+    }
+    tempStr = tempStr.split('');
+    let index = 0;
+    tempField = [];
+    for (let i = 0; i < 3; i += 1) {
+      tempField[i] = [];
+      for (let j = 0; j < 3; j += 1) {
+        tempField[i][j] = parseInt(tempStr[index]);
+        index += 1;
+      }
+    }
+    assert.deepEqual(tempField, controller.getField());
+  }
+});
 
+Given('поле {string}', (str) => {
+  let tempStr = '';
+  for (const s of str) {
+    if (s !== '|') tempStr += s;
+  }
+  tempStr = tempStr.split('');
+  let index = 0;
+  tempField = [];
+  for (let i = 0; i < 3; i += 1) {
+    tempField[i] = [];
+    for (let j = 0; j < 3; j += 1) {
+      tempField[i][j] = parseInt(tempStr[index]);
+      index += 1;
+    }
+  }
+  controller.presetField(tempField);
+});
+
+Then('возвращается ошибка', () => {
+  controller.getStatus();
+});
+
+Then('победил игрок {int}', (int) => {
+  controller.win(tempField, int);
 });
--- src/game.js
+++ src/game.js
@@ -1,17 +1,58 @@
 let field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 // eslint-disable-next-line no-unused-vars
 let currentPlayer = 1;
+let currentWinner = 0;
+let status = 'Ok';
 
 function getField() {
   return field;
 }
+function setStatus(string) {
+  status = string;
+}
+function getStatus() {
+  return status;
+}
 
-function makeMove(x, y) {
-  field[y][x] = 1;
+function win(field, currentPlayer) {
+  // строки
+  for (let row = 0; row < 3; row += 1) {
+    if (field[row][0] === field[row][1] && field[row][0] === field[row][2]) {
+      currentWinner = field[row][0];
+      return currentWinner === currentPlayer;
+    }
+  }
+  // колонки
+  for (let column = 0; column < 3; column += 1) {
+    if (field[0][column] === field[1][column] && field[0][column] === field[2][column]) {
+      currentWinner = field[0][column];
+      return currentWinner === currentPlayer;
+    }
+  }
+  // диагональ
+  if (field[0][0] === field[1][1] && field[0][0] === field[2][2]) {
+    currentWinner = field[0][0];
+    return currentWinner === currentPlayer;
+  }
+  // диагональ
+  if (field[2][0] === field[1][1] && field[2][0] === field[0][2]) {
+    currentWinner = field[2][0];
+    return currentWinner === currentPlayer;
+  }
 }
 
 function reset() {
   field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+}
+
+function makeMove(x, y) {
+  if (field[y][x] === 0) {
+    field[y][x] = currentPlayer;
+    setStatus('Ok');
+    //if (win(field, currentPlayer)) reset();
+  } else {
+    setStatus('Ошибка');
+  }
 }
 
 function presetField(newField) {
@@ -28,4 +69,7 @@ module.exports = {
   reset,
   presetField,
   setCurrentPlayer,
+  setStatus,
+  getStatus,
+  win,
 };
